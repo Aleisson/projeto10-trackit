6452d96a246b797a1d21c3f62a28d586e6a6441a
Aleisson <caioaleisson@gmail.com>
2022-08-06 15:30:58 -0300

    BrowserRouter - App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+
+
 function App(){
 
 
     return(
         <>
-            <h1>App teste</h1>
+            <BrowserRouter>
+                <Routes>
+                    {/* <Route path='/' element={<Login />} */}
+                </Routes>
+            </BrowserRouter>
         </>
     );
 
