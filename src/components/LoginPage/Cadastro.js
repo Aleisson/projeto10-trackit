15db08ec6fc1fd8bae040292928ee28b1ed6f01c
Aleisson <caioaleisson@gmail.com>
2022-08-07 01:50:04 -0300

    post Cad

--- src/components/LoginPage/Cadastro.js
+++ src/components/LoginPage/Cadastro.js
@@ -1,24 +1,65 @@
 import styled from "styled-components";
-import icon from "../../img/iconTrackIt.png";
-import {LoginPage, LoginForm} from "./style";
 import { Link } from 'react-router-dom';
-import { ThreeDots  } from  'react-loader-spinner';
+import { ThreeDots } from 'react-loader-spinner';
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+import { LoginPage, LoginForm } from "./style";
+import icon from "../../img/iconTrackIt.png";
+import {postCad} from "../../services/tracklit"
 
 function Cadastro() {
 
-    function cadastroLogin(event){    
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const [form, setForm] = useState({});
+
+    function handleForm({ value, name }) {
+
+        setForm({
+            ...form,
+            [name]: value,
+        });
+
+        // console.log(form);
+
+
+    }
+
+
+    function cadastroPost(event) {
         event.preventDefault();
+
+        const promise = postCad({email: form.email, name: form.name, image: form.image, password: form.password});
+
+        promise.then(() => navigate("/"));
+
+        promise.catch(() => alert("Usuario já cadastrado"));
+
+
     }
 
     return (
         <CadastroPage>
             <img src={icon} alt="ICON" />
-            <CadastroForm onSubmit={cadastroLogin}>
-                <input type='email' placeholder='email' required />
-                <input type='password' placeholder='senha' required/>
-                <input type='text' placeholder='nome' required/>
-                <input type='url' placeholder='foto' required/>
-                <button type="submit">Cadastrar</button>
+            <CadastroForm onSubmit={cadastroPost}>
+                <input type='email' placeholder='email' name="email" onChange={(e) => handleForm({
+                    name: e.target.name,
+                    value: e.target.value,
+                })} required disabled={loading}/>
+                <input type='password' placeholder='senha' name="password" onChange={(e) => handleForm({
+                    name: e.target.name,
+                    value: e.target.value,
+                })} required disabled={loading}/>
+                <input type='text' placeholder='nome' name="name" onChange={(e) => handleForm({
+                    name: e.target.name,
+                    value: e.target.value,
+                })} required disabled={loading}/>
+                <input type='url' placeholder='foto' name="image" onChange={(e) => handleForm({
+                    name: e.target.name,
+                    value: e.target.value,
+                })} required disabled={loading}/>
+                <button type="submit">{loading ? Dots : "Cadastrar"}</button>
             </CadastroForm>
             <Link to="/">
                 <p>Já tem uma conta? Faça login!</p>
@@ -38,13 +79,13 @@ const CadastroForm = styled(LoginForm)`
     height: 300px;
 `;
 
-const Dots = <ThreeDots 
-        height="80" 
-        width="80" 
-        radius="9"
-        color="#ffffff" 
-        ariaLabel="three-dots-loading"
-        wrapperStyle={{}}
-        wrapperClassName=""
-        visible={true}
- />
+const Dots = <ThreeDots
+    height="80"
+    width="80"
+    radius="9"
+    color="#ffffff"
+    ariaLabel="three-dots-loading"
+    wrapperStyle={{}}
+    wrapperClassName=""
+    visible={true}
+/>
