09a0aae03ec469e0940dfda0cab8592bca591d63
Aleisson <caioaleisson@gmail.com>
2022-08-06 21:46:07 -0300

    login  event input

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -1,26 +1,47 @@
 import icon from "../../img/iconTrackIt.png";
-import {LoginPage, LoginForm} from "./style";
+import { LoginPage, LoginForm } from "./style";
+import { useState } from "react";
 import { Link } from 'react-router-dom';
-import { ThreeDots  } from  'react-loader-spinner';
+import { ThreeDots } from 'react-loader-spinner';
 
 function Login() {
 
-    function accessLogin(event){    
+    const [form, setForm] = useState({});
+
+    function handleForm({ value, name }) {
+        
+        setForm({
+            ...form,
+            [name]: value,
+        });
+
+        
+    }
+
+    function accessLogin(event) {
         event.preventDefault();
+        console.log("email " + form.email);
+        console.log("Password " + form.password);
     }
 
     return (
         <LoginPage>
             <img src={icon} alt="ICON" />
             <LoginForm onSubmit={accessLogin}>
-                <input type='email' placeholder='email' required/>
-                <input type='password' placeholder='senha' required/>
+                <input type='email' placeholder='email' name="email" onChange={(e) => handleForm({
+                    name: e.target.name,
+                    value: e.target.value,
+                })} required />
+                <input type='password' placeholder='senha'  name="password" onChange={(e) => handleForm({
+                    name: e.target.name,
+                    value: e.target.value,
+                })}  required />
                 <button type="submit">Entrar</button>
             </LoginForm>
             <Link to="/cadastro">
                 <p>Não tem uma conta? Cadastre-se!</p>
             </Link>
-            
+
         </LoginPage>
     )
 
@@ -28,14 +49,14 @@ function Login() {
 
 export default Login;
 
-const Dots = <ThreeDots 
-        height="80" 
-        width="80" 
-        radius="9"
-        color="#ffffff" 
-        ariaLabel="three-dots-loading"
-        wrapperStyle={{}}
-        wrapperClassName=""
-        visible={true}
- />
+const Dots = <ThreeDots
+    height="80"
+    width="80"
+    radius="9"
+    color="#ffffff"
+    ariaLabel="three-dots-loading"
+    wrapperStyle={{}}
+    wrapperClassName=""
+    visible={true}
+/>
 
